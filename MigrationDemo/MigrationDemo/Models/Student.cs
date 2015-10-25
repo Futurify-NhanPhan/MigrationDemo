a2be626d14f9904169c45443baa381c71d397c40
Nhan Phan <nhanphan@futurify.vn>
2015-10-25 11:49:37 +0700

    afdslkj

--- MigrationDemo/MigrationDemo/Models/Student.cs
+++ MigrationDemo/MigrationDemo/Models/Student.cs
@@ -11,6 +11,7 @@ namespace MigrationDemo.Models
         public int Name { get; set; }
         public DateTime BirdDate { get; set; }
         public string Address { get; set; }
-
+fadsfa123
+f
     }
 }
